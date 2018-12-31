a4129d00df19361bcd09c895b0b59c991ff7ce9f
Superrusp <amir.dogmosh@gmail.com>
2018-12-31 21:08:24 +0200

    created annotation

--- lab_1/src/main/java/fillers/Generator.java
+++ lab_1/src/main/java/fillers/Generator.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to mark methods that fill the array.
+ *
+ * @author Amir Dogmosh
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Generator {
